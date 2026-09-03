0398b3dd854594412fe7e08e47f47d9719128aac
newbd <super.ark81@gmail.com>
2026-09-02 22:30:02 -0700

    Update kitty.py
    Made it so the user can edit how many times they wanna meow

--- kitty.py
+++ kitty.py
@@ -1,2 +1,11 @@
-for i in range(3):
+while True:
+    num = int(input("How many times do you want to meow?: "))
+
+    if num < 0:
+        print("You cant pick a negative numberm try picking a normal positive one! <3")
+        continue
+    break
+
+
+for _ in range(num):
     print("meow")
