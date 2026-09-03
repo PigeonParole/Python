e537f0ca80067c2a19d57624b3b3ac7057f1b834
newbd <super.ark81@gmail.com>
2026-09-02 19:49:18 -0700

    Add comments
    Comments

--- coin.py
+++ coin.py
@@ -1,14 +1,15 @@
 import time
 import random
-
+#asks the user if they wanna flip the coin
 choice = input("Welcome to the coin flipper! Do you want to flip a coin? (Y/N): ")
 if choice.upper() == "N":
     print("Ok")
+#flips the coin if user says yes
 elif choice.upper() == "Y":
     print("Flipping...")
     time.sleep(1)
     number = random.randint(1, 2)
-
+#Gives final result
     if number == 2:
         print("Heads")
     else:
